b233dc517ab03e60c82b04c29f68689b8300fe67
Muhammad Adil <muhammadadeel20300@gmail.com>
2025-03-14 15:09:48 +0500

    done

---
+++ kjcj.js
@@ -0,0 +1,58 @@
+// function adjective(){
+//     let a="Creazy"
+//     let b= "Amazing"
+//     let c= "Fire"
+// let d=Math.floor(Math.random()*3)
+// if(d==0){return a}
+// else if(d==1){return b}
+// else if(d==2){return c}
+// }
+// function shopName(){
+//     let a="Engine"
+//     let b= "Foods"
+//     let c= "Garments"
+// let d=Math.floor(Math.random()*3)
+// if(d==0){return a}
+// else if(d==1){return b}
+// else if(d==2){return c}
+// }
+// function anotherWord(){
+//     let a="Bros"
+//     let b= "Limited"
+//     let c= "Hub"
+// let d=Math.floor(Math.random()*3)
+// if(d==0){return a}
+// else if(d==1){return b}
+// else if(d==2){return c}
+// }
+
+// console.log("Business Name\n" , adjective(),shopName(),anotherWord() )
+
+
+
+// let arr  =  ['Iqra','adil','hussain','Asma']
+
+// // arr[3] = "Adil";
+
+// // console.log(arr[4])
+
+// // console.log(arr.toString())
+// console.log(arr.pop())
+// console.log(arr)
+
+// console.log(arr.push("lover"))
+// console.log(arr)
+// console.log(arr.shift())
+// console.log(arr)
+
+// console.log(arr.unshift())
+// console.log(arr)
+
+
+// For in loop 
+
+let arr =  [1, 2, 3, 4, 5, 55]
+ 
+arr.forEach((value , index , arr) => {
+    console.log(value,"value thi", index ,'index tha', arr)
+});
